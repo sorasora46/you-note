c14d0dbe3f13eb74e6bcc56a94fd877fac814f44
sorasora46 <sorrawit456789@gmail.com>
2024-10-27 00:47:16 +0700

    Init backend

---
+++ backend/src/main/java/me/sora/younote/YounoteApplication.java
@@ -0,0 +1,13 @@
+package me.sora.younote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YounoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YounoteApplication.class, args);
+	}
+
+}
